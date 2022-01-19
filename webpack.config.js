9e7f8dde70ae0b9dab4b5d6d9606d140d447d5a2
Edward <v-tazho@hotmail.com>
2022-01-19 13:40:45 +0800

    11

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,9 @@ const devCerts = require("office-addin-dev-certs");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const CustomFunctionsMetadataPlugin = require("custom-functions-metadata-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const glob = require("glob");
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlProd = "https://www.excel.meekou.cn/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
 
 /* global require, module, process, __dirname */
 
@@ -14,17 +14,27 @@ async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
   return { cacert: httpsOptions.ca, key: httpsOptions.key, cert: httpsOptions.cert };
 }
-
+//const meekou = glob.sync("./src/shared/AppConsts.ts");
 module.exports = async (env, options) => {
   const dev = options.mode === "development";
   const buildType = dev ? "dev" : "prod";
   const config = {
     devtool: "source-map",
     entry: {
+      // meekou: [
+      //   "./src/shared/appconsts.ts",
+      //   "./src/shared/meekouconsts.ts",
+      //   "./src/shared/dialoginput.ts",
+      //   "./src/services/meekouapi.ts",
+      //   "./src/services/common.model.ts",
+      //   "./src/services/http.ts",
+      // ],
       polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
       functions: "./src/functions/functions.ts",
       taskpane: "./src/taskpane/taskpane.ts",
       commands: "./src/commands/commands.ts",
+      login: "./src/login/login.ts",
+      dataFromWeb: "./src/pages/data-from-web/data-from-web.ts",
       dialog: "./src/dialog/dialog.ts",
     },
     output: {
@@ -76,6 +86,16 @@ module.exports = async (env, options) => {
         chunks: ["polyfill", "taskpane", "commands", "functions"],
       }),
       new HtmlWebpackPlugin({
+        filename: "login.html",
+        template: "./src/login/login.html",
+        chunks: ["polyfill", "login"],
+      }),
+      new HtmlWebpackPlugin({
+        filename: "data-from-web.html",
+        template: "./src/pages/data-from-web/data-from-web.html",
+        chunks: ["polyfill", "dataFromWeb"],
+      }),
+      new HtmlWebpackPlugin({
         filename: "dialog.html",
         template: "./src/dialog/dialog.html",
         chunks: ["polyfill", "dialog"],
@@ -83,7 +103,7 @@ module.exports = async (env, options) => {
       new CopyWebpackPlugin({
         patterns: [
           {
-            from: "assets/*",
+            from: "assets/icon-*",
             to: "assets/[name][ext][query]",
           },
           {
